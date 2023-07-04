d305febdaae68b2a9ff5d6ce90250e53614b8228
Kai <kai.zahn@yahoo.de>
2023-07-04 09:02:03 +0200

    Other * Added the lang entries from Fabric (needed for Jade etc.) * Not sure where its used but:  - set temperature, viscosity and density to lava value * Added own textures to avoid problems with tint - Basically just grayscale minecraft lava texture

--- src/main/java/com/commandert3706/createfoundry/fluid/BaseFluidType.java
+++ src/main/java/com/commandert3706/createfoundry/fluid/BaseFluidType.java
@@ -16,17 +16,14 @@ import java.util.function.Consumer;
 public class BaseFluidType extends FluidType {
     private final ResourceLocation stillTexture;
     private final ResourceLocation flowingTexture;
-    private final ResourceLocation overlayTexture;
     private final int tintColor;
     private final Vector3f fogColor;
 
-    public BaseFluidType(final ResourceLocation stillTexture, final ResourceLocation flowingTexture,
-                         final ResourceLocation overlayTexture, final int tintColor, final Vector3f fogColor,
+    public BaseFluidType(final ResourceLocation stillTexture, final ResourceLocation flowingTexture, final int tintColor, final Vector3f fogColor,
                          final Properties properties) {
         super(properties);
         this.stillTexture = stillTexture;
         this.flowingTexture = flowingTexture;
-        this.overlayTexture = overlayTexture;
         this.tintColor = tintColor;
         this.fogColor = fogColor;
     }
@@ -37,10 +34,6 @@ public class BaseFluidType extends FluidType {
 
     public ResourceLocation getFlowingTexture() {
         return flowingTexture;
-    }
-
-    public ResourceLocation getOverlayTexture() {
-        return overlayTexture;
     }
 
     public int getTintColor() {
@@ -62,11 +55,6 @@ public class BaseFluidType extends FluidType {
             @Override
             public ResourceLocation getFlowingTexture() {
                 return flowingTexture;
-            }
-
-            @Override
-            public ResourceLocation getOverlayTexture() {
-                return overlayTexture;
             }
 
             @Override
